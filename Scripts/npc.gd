675872e756d30838a2d6af82d40f3f935af6170f
Cryp3rone <Manach.Nicolas.mn@gmail.com>
2025-01-23 10:20:58 +0100

    npc (still not functional)

--- Scripts/npc.gd
+++ Scripts/npc.gd
@@ -4,6 +4,10 @@ var is_chatting = false
 var player_in_chat_zone = false
 var player
 
+func _process(delta: float) -> void:
+	if Input.is_action_just_pressed("Chat"):
+		is_chatting = true
+
 func _on_chat_detection_area_body_entered(body: Node2D) -> void:
 	if body.has_method("player"):
 		player = body
